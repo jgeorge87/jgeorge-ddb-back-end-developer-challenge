fe1fd72872f49fd2c49d9fd3f9b4f7ce6637a5be
Jeremiah George <jgeorge8719@gmail.com>
2020-09-15 18:48:29 -0400

    Master changes?

---
+++ BrivTest2/Models/Modifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrivTest2.Models
+{
+    public class Modifier
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+    }
+}
